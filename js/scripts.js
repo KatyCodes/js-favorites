d9f18fbbcb57cb4e89eaa3511779bb25d6893c37
Katy Henning and Kevin Johnston <katy@>
2016-08-15 10:17:25 -0700

    add javascript to create and reorder the array

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,20 @@ $(document).ready(function() {
     event.preventDefault();
 
     var nameInput = $("input#name").val();
+    var candyInput = $("input#candy").val();
+    var colorInput = $("input#color").val();
+    var animalInput = $("input#animal").val();
+
+    var result = [candyInput, colorInput, animalInput]
+    var newResult = [result[1],result[0],result[2]]
+        console.log(result);
+        console.log(newResult);
 
     if (nameInput === "") {
       $(".error").show();
+      // $(result).hide();
     } else {
+      // $(result).show();
       $(".error").hide();
     };
 
